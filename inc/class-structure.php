99f1f6aa23e7a09d9eaf0362321c67ce55757181
Philip John <phil@philipjohn.co.uk>
2014-10-26 17:02:23 +0000

    Story CPT

--- inc/class-structure.php
+++ inc/class-structure.php
@@ -1,16 +1,30 @@
 <?php
 
+/**
+ * Class Storyteller_Structure
+ *
+ * Sets up structural requirements of the Storyteller plugin
+ */
 Class Storyteller_Structure extends Storyteller_Plugin {
 
+	/**
+	 * Uses parent Plugin class to set up locale and other useful bits.
+	 *
+	 * @throws exception
+	 */
 	function __construct() {
 
 		$this->setup( 'storyteller', 'plugin' );
 		$this->version = 0.1;
 
-		$this->add_action('init');
+		$this->add_action( 'init' );
+		$this->add_action( 'init', 'custom_post_types' );
 
 	}
 
+	/**
+	 * Warn users if they are missing the Dependencies plugin
+	 */
 	public function init() {
 
 		if ( ! class_exists('Plugin_Dependencies') ) {
@@ -19,6 +33,30 @@ Class Storyteller_Structure extends Storyteller_Plugin {
 
 	}
 
+	public function custom_post_types() {
+
+		// Stories
+		register_extended_post_type(
+			'story',
+			array(
+				'cols' => array(
+					'published' => array(
+						'title' => __('Published'),
+						'meta_key' => 'published_date',
+						'date_format' => get_option('date_format'),
+					),
+				),
+			),
+			array(
+				'singular' => __('Story'),
+				'plural' => __('Stories'),
+				'slug' => __('stories'),
+			)
+		);
+
+	}
+
 }
 
+/** @var Storyteller_Structure $Storyteller_Structure Initiates the structure class */
 $Storyteller_Structure = new Storyteller_Structure();
